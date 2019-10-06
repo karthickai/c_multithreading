957631b5d9129b9dd903d024b0d2ba44e3e4959e
Karthick Pannerselvam <karthickps.ai@gmail.com>
2019-10-06 19:00:05 +0530

    Changed file name and update in Thread

---
+++ producer_consumer/thread_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> // Multithreading library
+
+#define NUM_OF_THREAD 5
+
+pthread_t tid[NUM_OF_THREAD];
+void *arg;
+int i;
+int identity[NUM_OF_THREAD];
+
+void greetings(void *arg) {
+    int *val = (int*)arg;
+    int id = *val;
+    printf("%s id = %d \n", "Thread Started", id);
+    pthread_exit(0);
+}
+
+int main() {
+    printf("%s\n", "Producer Consumer");
+    for(i=0; i< NUM_OF_THREAD; i++){
+        identity[i] = i;
+        pthread_create(&tid[i], NULL, greetings, &identity[i]);
+    }
+
+    for(i=0; i< NUM_OF_THREAD; i++){
+        pthread_join(tid[i], NULL);
+    }
+    
+    return 0;
+}
